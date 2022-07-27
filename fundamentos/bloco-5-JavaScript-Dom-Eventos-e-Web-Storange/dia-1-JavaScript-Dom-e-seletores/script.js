efeea8a6af5529a3a5357630bb493d0dc8f6a29f
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-07-27 20:12:58 -0300

    commit dos requisitos 1 , 2 e 3 (Empolguei e esqueci de comitar)

---
+++ fundamentos/bloco-5-JavaScript-Dom-Eventos-e-Web-Storange/dia-1-JavaScript-Dom-e-seletores/script.js
@@ -0,0 +1,18 @@
+function mudarTagP(texto) {
+    document.getElementById("textoPrincipal").innerText = texto;  
+} 
+mudarTagP('Trabalhando em uma empresa do exterior!');
+
+function mudarCorLaterais(cor) {
+    let laterais = document.getElementsByClassName('main-content');
+    for (let index = 0; index < laterais.length; index+=1) {
+        laterais[index].style.backgroundColor = cor;
+        
+    }
+}
+mudarCorLaterais('rgb(76,164,109');
+
+function mudarCorCentro(cor) {
+    document.getElementsByClassName('center-content')[0].style.backgroundColor = cor;
+} mudarCorCentro('white')
+
