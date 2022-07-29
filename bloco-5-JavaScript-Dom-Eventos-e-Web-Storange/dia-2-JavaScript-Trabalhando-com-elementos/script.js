7a13e5e576279d6a797008f8626e1f4eeef2c05e
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-07-28 21:58:15 -0300

    Criando um h1 no body via javascript/Primeiro requisito

---
+++ bloco-5-JavaScript-Dom-Eventos-e-Web-Storange/dia-2-JavaScript-Trabalhando-com-elementos/script.js
@@ -0,0 +1,8 @@
+//Oq quero introduzir
+let fraseh1 = 'Exercício 5.2 - JavaScript Dom';
+//Onde quero introduzir
+let fraseExercicioH1 = document.createElement('h1');
+//Introduzindo
+fraseExercicioH1.innerText = fraseh1;
+
+document.body.appendChild(fraseExercicioH1);
