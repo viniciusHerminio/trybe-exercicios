00989c94124760050d0a87aa5c73fbc0eefb8884
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-07-28 23:51:17 -0300

    criando uma lista não ordenada filha da terceira section com os numeros de 1 a 10 por extenso/requisito 8

--- bloco-5-JavaScript-Dom-Eventos-e-Web-Storange/dia-2-JavaScript-Trabalhando-com-elementos/script.js
+++ bloco-5-JavaScript-Dom-Eventos-e-Web-Storange/dia-2-JavaScript-Trabalhando-com-elementos/script.js
@@ -30,3 +30,16 @@ let img = document.createElement('img');
 section2loc.appendChild(img);
 document.querySelector('.left-content').firstElementChild.classList.add('small-image');
 document.querySelector('.small-image').src="https://picsum.photos/200";
+
+let ul = document.createElement('ul');
+section3loc.appendChild(ul);
+document.querySelector('.rigth-content').firstElementChild.classList.add('listaNaoOrdenada');
+
+let listaNumeros = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
+for (let index = 0; index < listaNumeros.length; index+=1) {
+    let element = listaNumeros[index]; 
+    let li = document.createElement('li');
+    li.innerText = element;
+    ul.appendChild(li);
+}
+
