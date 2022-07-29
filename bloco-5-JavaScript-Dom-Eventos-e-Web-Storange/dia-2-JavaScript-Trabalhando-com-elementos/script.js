a39ce312605cf710db2f0b160fc2118e2fce604a
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-07-28 22:50:56 -0300

    acicionando tag section como filho da main e atribuindo uma classe a ela/requisito 3

--- bloco-5-JavaScript-Dom-Eventos-e-Web-Storange/dia-2-JavaScript-Trabalhando-com-elementos/script.js
+++ bloco-5-JavaScript-Dom-Eventos-e-Web-Storange/dia-2-JavaScript-Trabalhando-com-elementos/script.js
@@ -6,3 +6,9 @@ document.body.appendChild(fraseExercicioH1);
 let main = document.createElement('main');
 document.body.appendChild(main);
 document.querySelector('main').classList.add('main-content');
+
+let section = document.querySelector('.main-content');
+let section2 = document.createElement('section');
+section.appendChild(section2);
+document.querySelector('section').classList.add('center-content');
+
