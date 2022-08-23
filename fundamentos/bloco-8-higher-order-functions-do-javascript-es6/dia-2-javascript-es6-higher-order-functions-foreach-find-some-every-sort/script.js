1ee9420cb17ae7dcf3012892fa52581681bf65de
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-08-22 21:40:59 -0300

    realizando segunda atividade usando a função forEach

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -63,5 +63,19 @@ const books = [
   
   // Adicione o código do exercício aqui:
 
+  //  1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+
   const authorBornIn1947 = () => books.find((book) => book.author.birthYear === 1947);
-  console.log(authorBornIn1947());
+  //console.log(authorBornIn1947());
+
+  // 2 - Retorne o nome do livro de menor nome.
+let nameBook = books[0].name;
+const smallerName = () => {
+  books.forEach((book) => {
+    if (nameBook.length > book.name.length) {
+      nameBook = book.name;
+    }
+  });
+  return nameBook;
+}
+ console.log(smallerName());
